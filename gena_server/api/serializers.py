8a26b4d823a6407ceb7184d341fcca3071eb3516
Sergey <mserg04@gmail.com>
2025-03-28 12:41:53 +0400

    fixed password hashing serializers.py

--- gena_server/api/serializers.py
+++ gena_server/api/serializers.py
@@ -8,14 +8,19 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create(**validated_data)
+        # user = User.objects.create(**validated_data)
+        user = User.objects.create(
+                email=validated_data.get('email', ''),
+                userName=validated_data.get('userName', '')
+            )
+        user.set_password(validated_data['password'])
         return user
 
     def update(self, instance, validated_data):
         instance.email = validated_data.get('email', instance.email)
         instance.userName = validated_data.get('userName', instance.userName)
         if 'password' in validated_data:
-            instance.password = validated_data['password']
+            instance.set_password(validated_data['password'])
         instance.save()
         return instance
 
